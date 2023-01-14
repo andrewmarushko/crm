fefafaab23c97ec364b36ddee406b0cbfdf75e89
Andrew Marushko <andrewmarushko@gmail.com>
2023-01-14 12:09:48 +0100

    add some plugins for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,4 @@ module.exports = {
     require("@tailwindcss/line-clamp"),
     require("@tailwindcss/aspect-ratio"),
   ],
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
-  },
 };
