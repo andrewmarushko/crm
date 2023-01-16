bd50da99a20f724d004d50c9718dec00c5136124
anton-ishchenkoo <antonishchenko@megadevllc.com>
2023-01-16 19:14:38 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
+import { DashboardRoutingModule } from './dashboard/dashboard-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
+import { ConfigService } from './dashboard/dashboard.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, AuthLayoutComponent, DashboardLayoutComponent],
+  imports: [BrowserModule, AppRoutingModule, DashboardRoutingModule],
+  providers: [ConfigService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
