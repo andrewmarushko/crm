d0c1617469eeff5e2cd678f398f11ec68c0972cb
Andrew Marushko <andrewmarushko@gmail.com>
2023-02-05 00:16:35 +0100

    provide example how to use data in component as observable

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,17 +1,38 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
-interface UserInterface {
-  username: string;
+export interface UserInterface {
+  email: string;
+  description?: string | null;
+  name?: string | null;
+  second_name?: string | null;
+  avatar_url?: string | null;
+  phone?: string | null;
+  id: number;
+  isArchived: boolean;
+  createDateTime: Date;
+  lastChangedDateTime: Date;
 }
+
+const initialState: UserInterface = {
+  email: 'test@gmail.com',
+  description: null,
+  name: null,
+  second_name: null,
+  avatar_url: null,
+  phone: null,
+  id: 1,
+  isArchived: false,
+  createDateTime: new Date(),
+  lastChangedDateTime: new Date(),
+};
 
 @Injectable()
 export class UserService {
-  user = new BehaviorSubject<UserInterface>({
-    username: '',
-  });
+  private data = new BehaviorSubject<UserInterface>(initialState);
+  user$: Observable<UserInterface> = this.data.asObservable();
 
   setUser(user: UserInterface) {
-    this.user.next(user);
+    this.data.next(user);
   }
 }
--- src/app/shared/ui/header/header.component.ts
+++ src/app/shared/ui/header/header.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserInterface, UserService } from '@shared/services/user.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  user$: Observable<UserInterface>;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.user$ = this.userService.user$;
+  }
+  setUser() {}
+}
