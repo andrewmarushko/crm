842709ebdc783ef9f7fb2a060ed0312ac6043918
Andrew Marushko <andrewmarushko@gmail.com>
2023-02-17 00:22:30 +0000

    create company form template

--- src/app/auth/components/verify-company/verify-company.component.ts
+++ src/app/auth/components/verify-company/verify-company.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-verify-company',
   templateUrl: './verify-company.component.html',
   styleUrls: ['./verify-company.component.scss'],
 })
-export class VerifyCompanyComponent {}
+export class VerifyCompanyComponent {
+  constructor(private fb: FormBuilder) {}
+
+  verifyCompanyForm = this.fb.group({
+    company_name: '',
+  });
+  onSubmitForm() {
+    console.log(this.verifyCompanyForm.value);
+  }
+}
