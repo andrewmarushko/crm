9a799254aebbf86350871fdd914eb88172e9091e
Andrew Marushko <andrewmarushko@gmail.com>
2023-02-18 23:11:52 +0000

    add guard for auth page for unverified company

--- src/app/auth/modules/auth-routing.module.ts
+++ src/app/auth/modules/auth-routing.module.ts
@@ -5,6 +5,7 @@ import { SignInComponent } from '@auth/components/sign-in/sign-in.component';
 import { ForgotPasswordComponent } from '@auth/components/forgot-password/forgot-password.component';
 import { SignUpComponent } from '@auth/components//sign-up/sign-up.component';
 import { VerifyCompanyComponent } from '@auth/components/verify-company/verify-company.component';
+import { AuthGuard } from '@auth/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -19,14 +20,17 @@ const routes: Routes = [
   {
     path: 'sign-in',
     component: SignInComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'sign-up',
     component: SignUpComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'forgot-password',
     component: ForgotPasswordComponent,
+    canActivate: [AuthGuard],
   },
 ];
 
